e1b2ba9373b82c3bee1706950209f9e87121fe2e
Interface iQ <interfaceiq@Interfaces-MacBook-Pro.local>
2025-08-23 05:56:34 +0100

    dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import LandingPage from './Pages/LandingPage'
 import Booking from './Pages/Booking'
